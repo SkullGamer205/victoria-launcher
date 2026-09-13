85bb524ab3fba8bf3f191ca869bf34df130be087
adelmonte <138840776+adelmonte@users.noreply.github.com>
2026-09-13 08:02:07 -0400

    Release 0.51.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "dev.victorialauncher"
         minSdk = 26
         targetSdk = 35
-        versionCode = 52
-        versionName = "0.50.1"
+        versionCode = 53
+        versionName = "0.51.0"
     }
 
     signingConfigs {
