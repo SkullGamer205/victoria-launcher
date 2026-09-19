a2e8df6de7741e44e5d9addf3c7938e13edcec92
adelmonte <138840776+adelmonte@users.noreply.github.com>
2026-09-19 08:49:35 -0400

    Release 0.64.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "dev.victorialauncher"
         minSdk = 26
         targetSdk = 35
-        versionCode = 73
-        versionName = "0.64.0"
+        versionCode = 74
+        versionName = "0.64.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
